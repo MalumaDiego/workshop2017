70127403bd5664ec6f00e1ad5a7895a6acb66e86
MalumaDiego <diegoalecl10@gmail.com>
2017-10-11 23:26:44 -0400

    resgistro de usuarios

--- webapp/resources/views/auth/login.blade.php
+++ webapp/resources/views/auth/login.blade.php
@@ -59,7 +59,30 @@
                     </div>
                     <div class="form-group">
                         <p class="text-muted text-center">Do not have an account?
-                            <a href="signup.html">Sign Up!</a>
+                        @guest
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                            @endguest
                         </p>
                     </div>
                 </form>
